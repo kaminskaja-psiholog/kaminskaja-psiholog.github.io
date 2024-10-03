55003c54c27520a7ce81e9d7ad73ffc3ba341dee
kaminskaja-psiholog <kaminskaja.psiholog>
2024-10-03 21:37:35 +0300

    Add explicit email

--- js/email-encoder.js
+++ js/email-encoder.js
@@ -1,10 +1,15 @@
+function x() {
+  return [[["kami", "nskaja"].join(""), "psiholog"].join("."), [["gma", "il"].join(""), "com"].join(".")].join("@");
+}
+
 function decode(subject) {
   let result = ["mai", "lto", ":"].join("");
-  result += [[["kami", "nskaja"].join(""), "psiholog"].join("."), [["gma", "il"].join(""), "com"].join(".")].join("@");
+  result += x();
   result += "?";
   result += ["subject", subject].join("=");
   return result;
 }
+
 function openMailer(element, subject) {
   let y = decode(subject);
   element.setAttribute("href", y);
